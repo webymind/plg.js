4818d28052858a64a5a73f33a1a5e41c2db0961a
Webymind <97634238+webymind@users.noreply.github.com>
2022-02-12 20:59:25 +0400

    Update plugins.js

--- plugins.js
+++ plugins.js
@@ -8,6 +8,10 @@
 /*------------------------------------------------------------------*/
 /*	09) COUNT TO
 /*------------------------------------------------------------------*/
+$('#nav').onePageNav({
+   currentClass: 'current',
+   changeHash: false,
+   scrollSpeed: 750
 
 !function(t){function e(t,e){return t.toFixed(e.decimals)}t.fn.countTo=function(e){return e=e||{},t(this).each(function(){function a(){s+=l,c++,n(s),"function"==typeof o.onUpdate&&o.onUpdate.call(f,s),c>=r&&(i.removeData("countTo"),clearInterval(d.interval),s=o.to,"function"==typeof o.onComplete&&o.onComplete.call(f,s))}function n(t){var e=o.formatter.call(f,t,o);i.text(e)}var o=t.extend({},t.fn.countTo.defaults,{from:t(this).data("from"),to:t(this).data("to"),speed:t(this).data("speed"),refreshInterval:t(this).data("refresh-interval"),decimals:t(this).data("decimals")},e),r=Math.ceil(o.speed/o.refreshInterval),l=(o.to-o.from)/r,f=this,i=t(this),c=0,s=o.from,d=i.data("countTo")||{};i.data("countTo",d),d.interval&&clearInterval(d.interval),d.interval=setInterval(a,o.refreshInterval),n(s)})},t.fn.countTo.defaults={from:0,to:0,speed:1e3,refreshInterval:100,decimals:0,formatter:e,onUpdate:null,onComplete:null}}(jQuery);
 
@@ -16,25 +20,6 @@
 /*------------------------------------------------------------------*/
 /*  NAV ANCHOR(ONE PAGE NAV PLUGIN)
 /*------------------------------------------------------------------*/
-
-/*
- * jQuery One Page Nav Plugin
- * http://github.com/davist11/jQuery-One-Page-Nav
- *
- * Copyright (c) 2010 Trevor Davis (http://trevordavis.net)
- * Dual licensed under the MIT and GPL licenses.
- * Uses the same license as jQuery, see:
- * http://jquery.org/license
- *
- * @version 3.0.0
- *
- * Example usage:
- * $('#nav').onePageNav({
- *   currentClass: 'current',
- *   changeHash: false,
- *   scrollSpeed: 750
- * });
- */
 
 ;(function($, window, document, undefined){
 
